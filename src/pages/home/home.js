429a5a5513db7c704c3e08ae75dba3cee59e5f09
Nikhil Agarwal <agarwalnikhil576@gmail.com>
2022-10-23 19:11:55 +0530

    fixing bugs

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -29,12 +29,12 @@ const Home = () => {
                         popularMovies.map(movie => (
                             <Link style={{textDecoration:"none",color:"white"}} to={`/movie/${movie.id}`} >
                                 <div className="posterImage">
-                                    <img src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} />
+                                    <img alt="CarouselImage" src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} />
                                 </div>
                                 <div className="posterImage__overlay">
                                     <div className="posterImage__title">{movie ? movie.original_title: ""}</div>
                                     <div className="posterImage__runtime">
-                                        {movie ? movie.release_date : ""}
+                                        {movie ? movie.release_date : ""} 
                                         <span className="posterImage__rating">
                                             {movie ? movie.vote_average :""}
                                             <i className="fas fa-star" />{" "}
